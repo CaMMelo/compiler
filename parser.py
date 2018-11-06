e6c21e4cf3ce723d440c3b433a8443d99669ef86
CaMMelo <cmelo.marcelo.pereira@gmail.com>
2018-11-06 17:07:12 -0200

    removed @property from parser rules

--- parser.py
+++ parser.py
@@ -18,6 +18,7 @@ class Parser:
     def __init__(self, filepath):
         
         self.lexer = Lexer(filepath)
+        self.symbol_table = {}
     
     def consume_token(self, token):
 
@@ -27,74 +28,66 @@ class Parser:
             raise ParseError(f'expected \'{token.value}\' but got \'{self.lexer.lexeme}\'.', 
                 self.lexer.x, self.lexer.y)
     
-    @property
     def function(self):
-        self.type
+        self.type()
         self.consume_token(Token.IDENT)
         self.consume_token(Token.ABREPAR)
-        self.arg_list
+        self.arg_list()
         self.consume_token(Token.FECHAPAR)
-        self.bloco
+        self.bloco()
     
-    @property
     def arg_list(self):
         if self.current_token in first['arg']:
-            self.arg
-            self.resto_arg_list
+            self.arg()
+            self.resto_arg_list()
         else:
             pass
     
-    @property
     def arg(self):
-        self.type
+        self.type()
         self.consume_token(Token.IDENT)
     
-    @property
     def resto_arg_list(self):
         if self.current_token == Token.VIRGULA:
             self.consume_token(Token.VIRGULA)
-            self.arg_list
+            self.arg_list()
         else:
             pass
 
-    @property
     def type(self):
         if self.current_token == Token.INT:
             self.consume_token(Token.INT)
         else:
             self.consume_token(Token.FLOAT)
     
-    @property
     def bloco(self):
         self.consume_token(Token.ABRECHAVE)
-        self.stmt_list
+        self.stmt_list()
         self.consume_token(Token.FECHACHAVE)
     
-    @property
     def stmt_list(self):
         if self.current_token in first['stmt']:
-            self.stmt
-            self.stmt_list
+            self.stmt()
+            self.stmt_list()
         else:
             pass
     
-    @property
     def stmt(self):
         if self.current_token in first['for_stmt']:
-            self.for_stmt
+            self.for_stmt()
         elif self.current_token in first['io_stmt']:
-            self.io_stmt
+            self.io_stmt()
         elif self.current_token in first['while_stmt']:
-            self.while_stmt
+            self.while_stmt()
         elif self.current_token in first['expr']:
-            self.expr
+            self.expr()
             self.consume_token(Token.PTOEVIRGULA)
         elif self.current_token in first['if_stmt']:
-            self.if_stmt
+            self.if_stmt()
         elif self.current_token in first['bloco']:
-            self.bloco
+            self.bloco()
         elif self.current_token in first['declaration']:
-            self.declaration
+            self.declaration()
         elif self.current_token == Token.BREAK:
             self.consume_token(Token.BREAK)
             self.consume_token(Token.PTOEVIRGULA)
@@ -103,52 +96,46 @@ class Parser:
             self.consume_token(Token.PTOEVIRGULA)
         elif self.current_token == Token.RETURN:
             self.consume_token(Token.RETURN)
-            self.fator
+            self.fator()
             self.consume_token(Token.PTOEVIRGULA)
         else:
             self.consume_token(Token.PTOEVIRGULA)
     
-    @property
     def declaration(self):
-        self.type
-        self.ident_list
+        self.type()
+        self.ident_list()
         self.consume_token(Token.PTOEVIRGULA)
     
-    @property
     def ident_list(self):
         
         self.consume_token(Token.IDENT)
-        self.resto_ident_list
+        self.resto_ident_list()
     
-    @property
     def resto_ident_list(self):
         if self.current_token == Token.VIRGULA:
             self.consume_token(Token.VIRGULA)
             self.consume_token(Token.IDENT)
-            self.resto_ident_list
+            self.resto_ident_list()
         else:
             pass
     
-    @property
     def for_stmt(self):
         self.consume_token(Token.FOR)
         self.consume_token(Token.ABREPAR)
-        self.opt_expr
+        self.opt_expr()
         self.consume_token(Token.PTOEVIRGULA)
-        self.opt_expr
+        self.opt_expr()
         self.consume_token(Token.PTOEVIRGULA)
-        self.opt_expr
+        self.opt_expr()
         self.consume_token(Token.FECHAPAR)
-        self.stmt
+        self.stmt()
     
-    @property
     def opt_expr(self):
         if self.current_token in first['expr']:
-            self.expr
+            self.expr()
         else:
             pass
     
-    @property
     def io_stmt(self):
         if self.current_token == Token.SCAN:
             self.consume_token(Token.SCAN)
@@ -161,16 +148,14 @@ class Parser:
         else:
             self.consume_token(Token.PRINT)
             self.consume_token(Token.ABREPAR)
-            self.out_list
+            self.out_list()
             self.consume_token(Token.FECHAPAR)
             self.consume_token(Token.PTOEVIRGULA)
     
-    @property
     def out_list(self):
-        self.out
-        self.resto_out_list
+        self.out()
+        self.resto_out_list()
     
-    @property
     def out(self):
         if self.current_token == Token.STRING:
             self.consume_token(Token.STRING)
@@ -181,190 +166,170 @@ class Parser:
         else:
             self.consume_token(Token.NUMFLOAT)
     
-    @property
     def resto_out_list(self):
         
         if self.current_token == Token.VIRGULA:
             self.consume_token(Token.VIRGULA)
-            self.out
-            self.resto_out_list
+            self.out()
+            self.resto_out_list()
         else:
             pass
     
-    @property
     def while_stmt(self):
         self.consume_token(Token.WHILE)
         self.consume_token(Token.ABREPAR)
-        self.expr
+        self.expr()
         self.consume_token(Token.FECHAPAR)
-        self.stmt
+        self.stmt()
     
-    @property
     def if_stmt(self):
         self.consume_token(Token.IF)
         self.consume_token(Token.ABREPAR)
-        self.expr
+        self.expr()
         self.consume_token(Token.FECHAPAR)
-        self.stmt
-        self.else_part
+        self.stmt()
+        self.else_part()
     
-    @property
     def else_part(self):
         if self.current_token == Token.ELSE:
             self.consume_token(Token.ELSE)
-            self.stmt
+            self.stmt()
         else:
             pass
     
-    @property
     def expr(self):
-        self.atrib
+        self.atrib()
     
-    @property
     def atrib(self):
-        a = self.or_
-        b = self.resto_atrib
+        a = self.or_()
+        b = self.resto_atrib()
         if not(a or b):
             raise ParseError(f'missing lvalue.', self.lexer.x, self.lexer.y)
     
-    @property
     def resto_atrib(self):
         if self.current_token == Token.ATRIB:
             self.consume_token(Token.ATRIB)
-            self.atrib
+            self.atrib()
         else:
             return True
         return False
     
-    @property
     def or_(self):
-        a = self.and_
-        b = self.resto_or
+        a = self.and_()
+        b = self.resto_or()
         return a and b
 
-    @property
     def resto_or(self):
         if self.current_token == Token.OR:
             self.consume_token(Token.OR)
-            self.and_
-            self.resto_or
+            self.and_()
+            self.resto_or()
         else:
             return True
         return False
     
-    @property
     def and_(self):
-        a = self.not_
-        b = self.resto_and
+        a = self.not_()
+        b = self.resto_and()
         return a and b
     
-    @property
     def resto_and(self):
         if self.current_token == Token.AND:
             self.consume_token(Token.AND)
-            self.not_
-            self.resto_and
+            self.not_()
+            self.resto_and()
         else:
             return True
         return False
     
-    @property
     def not_(self):
         if self.current_token == Token.NOT:
             self.consume_token(Token.NOT)
-            self.not_
+            self.not_()
         else:
-            return self.rel
+            return self.rel()
         return False
     
-    @property
     def rel(self):
-        a = self.add
-        b = self.resto_rel
+        a = self.add()
+        b = self.resto_rel()
         return a and b
     
-    @property
     def resto_rel(self):
         if self.current_token == Token.EQUAL:
             self.consume_token(Token.EQUAL)
-            self.add
+            self.add()
         elif self.current_token == Token.DIFF:
             self.consume_token(Token.DIFF)
-            self.add
+            self.add()
         elif self.current_token == Token.GT:
             self.consume_token(Token.GT)
-            self.add
+            self.add()
         elif self.current_token == Token.GTEQ:
             self.consume_token(Token.GTEQ)
-            self.add
+            self.add()
         elif self.current_token == Token.LT:
             self.consume_token(Token.GT)
-            self.add
+            self.add()
         elif self.current_token == Token.LTEQ:
             self.consume_token(Token.GTEQ)
-            self.add
+            self.add()
         else:
             return True
         return False
     
-    @property
     def add(self):
-        a = self.mult
-        b = self.resto_add
+        a = self.mult()
+        b = self.resto_add()
         return a and b
     
-    @property
     def resto_add(self):
 
         if self.current_token == Token.SOMA:
             self.consume_token(Token.SOMA)
-            self.mult
-            self.resto_add
+            self.mult()
+            self.resto_add()
         elif self.current_token == Token.SUBT:
             self.consume_token(Token.SUBT)
-            self.mult
-            self.resto_add
+            self.mult()
+            self.resto_add()
         else:
             return True
         return False
     
-    @property
     def mult(self):
-        a = self.uno
-        b = self.resto_mult
+        a = self.uno()
+        b = self.resto_mult()
         return a and b
     
-    @property
     def resto_mult(self):
         if self.current_token == Token.MULT:
             self.consume_token(Token.MULT)
-            self.uno
-            self.resto_mult
+            self.uno()
+            self.resto_mult()
         elif self.current_token == Token.DIV:
             self.consume_token(Token.DIV)
-            self.uno
-            self.resto_mult
+            self.uno()
+            self.resto_mult()
         elif self.current_token == Token.MODU:
             self.consume_token(Token.MODU)
-            self.uno
-            self.resto_mult
+            self.uno()
+            self.resto_mult()
         else:
             return True
         return False
     
-    @property
     def uno(self):
         if self.current_token == Token.SOMA:
             self.consume_token(Token.SOMA)
-            self.uno
+            self.uno()
         elif self.current_token == Token.SUBT:
             self.consume_token(Token.SUBT)
-            self.uno
+            self.uno()
         else:
-            return self.fator
+            return self.fator()
         return False
     
-    @property
     def fator(self):
         if self.current_token == Token.NUMINT:
             self.consume_token(Token.NUMINT)
@@ -375,15 +340,14 @@ class Parser:
             return True
         else:
             self.consume_token(Token.ABREPAR)
-            self.atrib
+            self.atrib()
             self.consume_token(Token.FECHAPAR)
         return False
         
-
     def parse(self):
         try:
             self.current_token = self.lexer.get_token()
-            self.function
+            self.function()
             self.consume_token(Token.EOF)
         except ParseError as error:
             print(error)
